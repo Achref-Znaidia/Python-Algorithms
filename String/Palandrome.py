dfec59da93dbc82c6a5e0c7ba71b2a60f0d3f5ba
Achref-Znaidia <achref.znaidia@gmail.com>
2026-01-08 01:50:50 +0100

    add_crc_alg

--- String/Palandrome.py
+++ String/Palandrome.py
@@ -1,11 +1,12 @@
-
-
-
 def check_string_is_palandrome(string: str) -> bool:
+    if type(string) is not str:
+        raise (TypeError)
     return string == string[::-1]
 
+
 def main():
-    check_string_palandrome("hahah")
+    print(check_string_is_palandrome("hahah"))
+
 
 if __name__ == "__main__":
     main()
